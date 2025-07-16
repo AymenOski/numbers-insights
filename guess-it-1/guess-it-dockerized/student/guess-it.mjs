502bde83db0f1177df789214f1a65ab677d82880
AymenOski <aymenazizi610@gmail.com>
2025-07-16 18:21:45 +0100

    calculating the probability of the next outcome

--- guess-it-1/guess-it-dockerized/student/guess-it.mjs
+++ guess-it-1/guess-it-dockerized/student/guess-it.mjs
@@ -1,61 +1,29 @@
-import { readFile } from 'fs/promises';
+import { stdin } from 'node:process';
+import { writeFile } from 'node:fs/promises';
 
-function sortArray(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = 0; j < arr.length - i - 1; j++) {
-      if(arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
+let data = [];
+
+stdin.setEncoding('utf8');
+
+stdin.on('data', async (chunk) => {
+  const lines = chunk.trim().split('\n');
+
+  for (let line of lines) { // ranging supposing the user inpus is like : 128\n123\n..
+    let num = parseInt(line.trim());
+    if (isNaN(num)) continue;
+
+    data.push(num);
+
+    const sum = data.reduce((acc, val) => acc + val, 0);
+    const avg = sum / data.length;
+
+    let variance = data.reduce((acc, val) => acc + ((val - avg) ** 2), 0) / data.length;
+
+    const stdDev = Math.sqrt(variance);
+
+    const x = Math.floor(avg - stdDev);
+    const y = Math.ceil(avg + stdDev);
+
+    await writeFile('output.txt' , `${x} ${y}`)
   }
-}
-
-const filePath = process.argv[2];
-
-if(!filePath) {
-  console.log("Please provide a file path as argument");
-  process.exit(1);
-}
-
-let fileContent = await readFile('data.txt', 'utf-8');
-// or (in case of .js file):
-// try {
-//   fileContent = readFileSync(filePath, 'utf-8');
-// } catch(err) {
-//   console.log(`Failed to open the file: ${filePath}`);
-//   process.exit(1);
-// }
-
-const lines = fileContent.trim().split('\n');
-
-const numbers = lines.map(line => parseInt(line.trim())).filter(num => !isNaN(num));
-
-let n = numbers.length;
-if(n === 0) {
-  console.log("No valid numbers found in the file");
-  process.exit(1);
-}
-// average 
-let sum = 0;
-for(let num of numbers) {
-  sum += num;
-}
-const average = sum / n;
-
-// variance
-let varianceSum = 0;
-for(let num of numbers) {
-  varianceSum += (num - average) ** 2;
-}
-const variance = varianceSum / n;
-
-// standard deviation
-const stdDev = Math.sqrt(variance);
-
-for(let num of numbers) {
-    let x = average - stdDev;
-    let y = average + stdDev;
-    console.log("[", x , "," , y ,"]");
-}
+});
